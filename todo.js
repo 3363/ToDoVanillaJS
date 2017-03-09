8af2195cc4534b0795fa03b6f7eb48172d28de4e
3363 <sylwia.codes@yandex.com>
2017-03-08 19:54:41 -0800

    Add basic html functionality and refactor todojs.

--- todo.js
+++ todo.js
@@ -2,24 +2,24 @@ var todoList = {
   todos: [
   ],
   displayTodos: function () {
-    if (this.todos.length  === 0) {
-      console.log("Your todos list is empty!");
+    if (this.todos.length === 0) {
+      console.log('Your todos list is empty!');
     } else {
-      console.log("My todos:")
+      console.log('My todos:');
       for (i = 0; i < this.todos.length; i++) {
         if (this.todos[i].completed) {
-          console.log("(x)", this.todos[i].todoText);  
+          console.log('(x)', this.todos[i].todoText);
         } else {
-          console.log("( )", this.todos[i].todoText);  
+          console.log('( )', this.todos[i].todoText);
         }
-      }      
+      }
     }
   },
   addTodo: function (todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
-    }),
+    });
     this.displayTodos();
   },
   changeTodo: function (position, todoText) {
@@ -30,12 +30,12 @@ var todoList = {
     this.todos.splice(position, 1);
     this.displayTodos();
   },
-  toggleCompleted: function(position){
+  toggleCompleted: function (position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
-  }, 
-  toggleAll: function() {
+  },
+  toggleAll: function () {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
     for (var i = 0; i < totalTodos; i++) {
@@ -49,8 +49,40 @@ var todoList = {
       }
     } else {
       for (var i = 0; i < totalTodos; i++)
-        this.todos[i].completed = true;
+      this.todos[i].completed = true;
     }
     this.displayTodos();
+  }
+};
+
+var handlers = {
+  displayTodos: function() {
+    todoList.displayTodos();
+  },
+  addTodo: function() {
+    var addTodoTextInput = document.getElementById("addTodoTextInput");
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = "";
+  },
+  changeTodo: function() {
+    var changeTodoPositionInput = document.getElementById("changeTodoPositionInput");
+    var changeTodoTextInput = document.getElementById("changeTodoTextInput");
+
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoTextInput.value = "";
+    changeTodoPositionInput.value = "";
+  },
+  deleteTodo: function() {
+    var deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
+    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+    deleteTodoPositionInput = "";
+  },
+  toggleCompleted: function() {
+    var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
+    todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+    toggleCompletedPositionInput.value = "";
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
   }
 };
